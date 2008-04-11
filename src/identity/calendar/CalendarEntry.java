9d14d2cd60d2dc2bb789f0603cec5d50503a6acb
jaremy <jaremy@fb416431-906a-47e0-9951-64936ecdd59a>
2008-04-11 18:37:05 +0000

    writing calendarDB files
    git-svn-id: https://onyx.boisestate.edu/repos/jaremy-vamsi/Calendar@19 fb416431-906a-47e0-9951-64936ecdd59a

--- src/identity/calendar/CalendarEntry.java
+++ src/identity/calendar/CalendarEntry.java
@@ -3,6 +3,7 @@
  */
 package identity.calendar;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -10,8 +11,12 @@ import java.util.Date;
  * @author jaremy
  *
  */
-public class CalendarEntry {
-
+public class CalendarEntry implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2165686360188303307L;
+	
 	/* Example Entry: 17#2006/11/4 4:55pm#public#The fun begins now#90# */
 	public int id;
 	public Date datetime;
@@ -19,6 +24,7 @@ public class CalendarEntry {
 	public String descr;
 	public int duration;
 	
+	public static DateFormat df = DateFormat.getDateTimeInstance();
 	
 	/**
 	 * CalendarEntry
@@ -26,7 +32,7 @@ public class CalendarEntry {
 	 * @param  
 	 * @return 
 	 */
-	public void CalendarEntry(int id, Date datetime, boolean isPublic, String descr, int duration ) {
+	CalendarEntry(int id, Date datetime, boolean isPublic, String descr, int duration ) {
 		this.id = id;
 		this.datetime = datetime;
 		this.isPublic = isPublic;
@@ -36,6 +42,22 @@ public class CalendarEntry {
 		// need to check and make sure this is correct.
 	}
 
-	
+	public static CalendarEntry parseStringArray( String[] p) {
+		try {
+			// parse the entry
+			int id = Integer.parseInt(p[0]);
+			Date datetime = df.parse(p[1]);
+			boolean isPublic = Boolean.parseBoolean(p[2]);
+			String descr = p[3];
+			int duration = Integer.parseInt(p[4]);
+			
+			return new CalendarEntry(id,datetime,isPublic,descr,duration);
+		} catch (Exception e) {
+			return null;
+		}
+		
+		// need to check and make sure this is correct.
+	}
+
 	
 }
